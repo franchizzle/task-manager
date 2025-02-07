0def5725a3c5b4859dad0771b66cfae5023a62c4
francesca <francheesey@gmail.com>
2025-02-06 21:56:21 -0800

    fix text

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -44,7 +44,7 @@ const App = () => {
                 <meta charSet="utf-8" />
                 <title>General Task (beta)</title>
                 <link rel="icon" href="/images/favicon.png" />
-                <script src="https://kit.fontawesome.com/af07b49f89.js" crossorigin="anonymous"></script>
+                <script src="https://kit.fontawesome.com/af07b49f89.js" crossOrigin="anonymous"></script>
                 <base target="_blank" />
                 <meta
                     content="Find focus like never before with the best free productivity tool on the market!"
